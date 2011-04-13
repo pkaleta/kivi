cf976a6d2e9dd03f82dfb8e5ebe23ddd644a3331
Piotr Kaleta <piotrek.kaleta@gmail.com>
2011-04-13 23:37:43 +0000

    Incomplete instantiate function.

--- Template.hs
+++ Template.hs
@@ -50,7 +50,43 @@ compile program = (stack, tiDumpInitial, heap, globals, tiStatInitial)
         mainAddress = aLookup globals "main" (error "Undefined function main.")
         stack = [mainAddress]
 
---eval :: TiState -> [TiState]
+eval :: TiState -> [TiState]
+eval state = state : restStates
+    where
+        restStates | tiFinal state = []
+                   | otherwise = eval nextState
+        nextState = doAdmin $ step state
+        doAdmin = applyToStats tiStatIncSteps
+
+step :: TiState -> TiState
+step (addr : restAddrs, _, heap, _, _) = dispatch $ hLookup heap addr
+    where
+        dispatch (NNum n) = numStep state n
+        dispatch (NSc name args body) = scStep state name args body
+        dispatch (nAp a1 a2) = apStep state a1 a2
+
+numStep :: TiState -> Int -> TiState
+numStep state n = error "Number at the top of the stack."
+
+apStep :: TiState -> Addr -> Addr -> TiState
+apStep (stack, dump, heap, globals, stats) a1 a2 =
+    (a1 : stack, dump, heap, globals, stats)
+
+scStep :: TiState -> Name -> [Name] -> CoreExpr
+scStep (stack0, dump, heap0, globals, stats)  name argNames body =
+    (stack1, dump, heap1, globals, stats)
+    where
+        stack1 = resultAddr : stack0
+        (heap1, resultAddr) = instantiate body heap env
+        env = argBindings ++ globals
+        argBindings = zip argNames getArgAddrs
+
+tiFinal :: TiState -> Bool
+tiFinal ([addr], _, heap, _, _) = isDataNode (hLookup heap addr)
+
+isDataNode :: Node -> Bool
+isDataNode (NNum n) = True
+isDataNode _ = False
 
 --showResults :: [TiState] -> String
 
@@ -64,3 +100,4 @@ allocateSc :: TiHeap -> CoreScDefn -> (TiHeap, (Name, Addr))
 allocateSc heap (name, args, body) = (heap1, (name, addr))
     where
         (heap1, addr) = hAlloc heap $ NSc name args body
+
