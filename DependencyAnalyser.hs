8d8995487e028a0db0600ea11421dfc6e60cc2d4
Piotr Kaleta <piotrek.kaleta@gmail.com>
2011-06-01 16:36:20 +0200

    Strongly connected components.

--- DependencyAnalyser.hs
+++ DependencyAnalyser.hs
@@ -4,7 +4,10 @@ module DependencyAnalyser where
 import Data.Set as Set
 
 
-dfs :: Ord a => (a -> [a]) -> (Set a, [a]) -> [a] -> (Set a, [a])
+dfs :: Ord a => (a -> [a]) -- returns the vertices available from the current vertex
+    -> (Set a, [a])        -- current state representing visited vertices and current path
+    -> [a]                 -- vertices to visit
+    -> (Set a, [a])        -- final state
 dfs succ = foldl (dfsHelper succ)
 
 
@@ -14,5 +17,26 @@ dfsHelper succ (vis, seq) v =
         True -> (vis, seq)  -- vertex has already been visited
         False ->            -- vertex was not visited yet
             (vis', v : seq')
-            where (vis', seq') = dfs succ (vis, seq) (succ v)
+            where (vis', seq') = dfs succ (Set.union vis $ Set.singleton v, seq) (succ v)
+
+
+spanDfs :: Ord a => (a -> [a]) -> (Set a, [Set a]) -> [a] -> (Set a, [Set a])
+spanDfs succ = foldl (spanDfsHelper succ)
+
+
+spanDfsHelper :: Ord a => (a -> [a]) -> (Set a, [Set a]) -> a -> (Set a, [Set a])
+spanDfsHelper succ (vis, setSeq) v =
+    case Set.member v vis of
+        True -> (vis, setSeq)
+        False ->
+            (vis', Set.fromList (v : seq) : setSeq)
+            where (vis', seq) = dfs succ (Set.union vis $ Set.singleton v, seq) (succ v)
+
+
+scc :: Ord a => (a -> [a]) -> (a -> [a]) -> [a] -> [Set a]
+scc ins outs vs =
+    snd topSortedSccs
+    where
+        topSortedVs = snd $ dfs outs (Set.empty, []) vs
+        topSortedSccs = spanDfs ins (Set.empty, []) topSortedVs
 
