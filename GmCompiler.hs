da65b661b7dfb2e8bc2026025a75f223d54c3f62
Piotr Kaleta <piotrek.kaleta@gmail.com>
2011-06-29 23:47:16 +0200

    Thrown away unnecessary trace.

--- GmCompiler.hs
+++ GmCompiler.hs
@@ -185,8 +185,8 @@ compileC (EAp e1 e2) env =
     compileC e2 env ++
     compileC e1 (argOffset 1 env) ++
     [Mkap]
-compileC (ESelect arity i v) env =
-    trace ("************* " ++ show v ++ show env) [Push $ aLookup env v $ error "dupa", Eval, Select arity i]
+compileC (ESelect r i v) env =
+    [Push $ aLookup env v $ error "dupa", Eval, Select r i]
 compileC (ELet isRec defs body) env | isRec = compileLetrec [Slide $ length defs] compileC defs body env
                                     | otherwise = compileLet [Slide $ length defs] compileC defs body env
 compileC (ECaseSimple expr alts) env =
