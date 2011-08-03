b23aea4ce547d90a74e7624ac153a9365bc67923
Piotr Kaleta <piotrek.kaleta@gmail.com>
2011-08-03 15:48:13 +0200

    Added arity information to generate proper LLVM code.

--- CodeGen.hs
+++ CodeGen.hs
@@ -88,73 +88,74 @@ mapScDefn heap template templates (name, addr) =
     setAttribute "body" body $ setAttribute "name" name template
     where
         (NGlobal arity code) = hLookup heap addr
-        body = trace ("\n\n" ++ show code ++ "\n\n") renderTemplates $ genScLLVMIR templates code
+        body = trace ("\n\n" ++ show code ++ "\n\n") renderTemplates $ genScLLVMIR templates arity code
 
 
-genScLLVMIR :: STGroup String -> GmCode -> [LLVMIR]
-genScLLVMIR templates code = ir
+genScLLVMIR :: STGroup String -> Int -> GmCode -> [LLVMIR]
+genScLLVMIR templates arity code = ir
     where
-        (reg, stack, ir) = foldl (\state@(reg, stack, ir) instr -> trace ("instr: " ++ show instr ++ "\nstack: " ++ show stack ++ "\n\n") (collectInstrLLVMIR templates state instr)) (initialReg, [], []) code
+        (reg, stack, ir) = foldl (\state@(reg, stack, ir) instr -> trace ("instr: " ++ show instr ++ "\nstack: " ++ show stack ++ "\n\n") (collectInstrLLVMIR templates arity state instr)) (initialReg, [], []) code
 
 
 collectInstrLLVMIR :: STGroup String
-               -> (Reg, LLVMStack, [LLVMIR])
-               -> Instruction
-               -> (Reg, LLVMStack, [LLVMIR])
-collectInstrLLVMIR templates (reg, stack, ir) (Update n) = (reg, stack, ir ++ [template'])
+                   -> Int
+                   -> (Reg, LLVMStack, [LLVMIR])
+                   -> Instruction
+                   -> (Reg, LLVMStack, [LLVMIR])
+collectInstrLLVMIR templates arity (reg, stack, ir) (Update n) = (reg, stack, ir ++ [template'])
     where
         Just template = getStringTemplate "update" templates
         template' = setManyAttrib [("n", show n)] template
-collectInstrLLVMIR templates (reg, stack, ir) (Push n) = (reg, stack, ir ++ [template'])
+collectInstrLLVMIR templates arity (reg, stack, ir) (Push n) = (reg, stack, ir ++ [template'])
     where
         Just template = getStringTemplate "push" templates
         template' = setManyAttrib [("n", show n)] template
-collectInstrLLVMIR templates (reg, stack, ir) (Pop n) = (reg, stack, ir ++ [template'])
+collectInstrLLVMIR templates arity (reg, stack, ir) (Pop n) = (reg, stack, ir ++ [template'])
     where
         Just template = getStringTemplate "pop" templates
         template' = setManyAttrib [("n", show n)] template
 -- TODO: change this not to allocate numbers on heap
-collectInstrLLVMIR templates (reg, stack, ir) (Pushint n) = (nextReg reg, stack, ir ++ [template'])
+collectInstrLLVMIR templates arity (reg, stack, ir) (Pushint n) = (nextReg reg, stack, ir ++ [template'])
     where
         Just template = getStringTemplate "pushint" templates
         template' = setManyAttrib [("n", show n)] template
-collectInstrLLVMIR templates (reg, stack, ir) (Pushglobal v) = (reg, stack, ir ++ [template'])
+collectInstrLLVMIR templates arity (reg, stack, ir) (Pushglobal v) = (reg, stack, ir ++ [template'])
     where
         Just template = getStringTemplate "pushglobal" templates
 -- TODO: fix arity
-        template' = setManyAttrib [("arity", show 1), ("name", "_" ++ v)] template
-collectInstrLLVMIR templates (reg, stack, ir) (Mkap) = (reg, stack, ir ++ [template])
+        template' = setManyAttrib [("arity", show arity), ("name", "_" ++ v)] template
+collectInstrLLVMIR templates arity (reg, stack, ir) (Mkap) = (reg, stack, ir ++ [template])
     where
         Just template = getStringTemplate "mkap" templates
---collectInstrLLVMIR templates (reg, stack, ir) (Unwind) =  (reg, stack, ir)
---    where
---        Just template = getStringTemplate "unwind" templates
-collectInstrLLVMIR templates (reg, stack, ir) (Eval) = (reg, stack, ir ++ [template])
+collectInstrLLVMIR templates arity (reg, stack, ir) (Unwind) =  (reg, stack, ir)
+    where
+        Just template = getStringTemplate "unwind" templates
+collectInstrLLVMIR templates arity (reg, stack, ir) (Eval) = (reg, stack, ir ++ [template])
     where
         Just template = getStringTemplate "eval" templates
-collectInstrLLVMIR templates (reg, stack, ir) (Pushbasic n) = (reg, stack, ir ++ [template'])
+collectInstrLLVMIR templates arity (reg, stack, ir) (Pushbasic n) = (reg, stack, ir ++ [template'])
     where
         Just template = getStringTemplate "pushbasic" templates
         template' = setManyAttrib [("n", show n)] template
-collectInstrLLVMIR templates (reg, stack, ir) (Get) = (reg, stack, ir ++ [template])
+collectInstrLLVMIR templates arity (reg, stack, ir) (Get) = (reg, stack, ir ++ [template])
     where
         Just template = getStringTemplate "get" templates
-collectInstrLLVMIR templates (reg, stack, ir) (Add) = (reg, stack, ir ++ [template])
+collectInstrLLVMIR templates arity (reg, stack, ir) (Add) = (reg, stack, ir ++ [template])
     where
         Just template = getStringTemplate "add" templates
-collectInstrLLVMIR templates (reg, stack, ir) (Sub) = (reg, stack, ir ++ [template])
+collectInstrLLVMIR templates arity (reg, stack, ir) (Sub) = (reg, stack, ir ++ [template])
     where
         Just template = getStringTemplate "sub" templates
-collectInstrLLVMIR templates (reg, stack, ir) (Mul) = (reg, stack, ir ++ [template])
+collectInstrLLVMIR templates arity (reg, stack, ir) (Mul) = (reg, stack, ir ++ [template])
     where
         Just template = getStringTemplate "mul" templates
-collectInstrLLVMIR templates (reg, stack, ir) (Div) = (reg, stack, ir ++ [template])
+collectInstrLLVMIR templates arity (reg, stack, ir) (Div) = (reg, stack, ir ++ [template])
     where
         Just template = getStringTemplate "div" templates
-collectInstrLLVMIR templates (reg, stack, ir) (MkInt) = (reg, stack, ir ++ [template])
+collectInstrLLVMIR templates arity (reg, stack, ir) (MkInt) = (reg, stack, ir ++ [template])
     where
         Just template = getStringTemplate "mkint" templates
-collectInstrLLVMIR templates state _ = state
+collectInstrLLVMIR templates arity state _ = state
 
 
 --updateNumLLVMIR :: STGroup String -> Name -> [LLVMIR]
