337723ed5a432eb437e7f43a7107ea2c40508133
Piotr Kaleta <piotrek.kaleta@gmail.com>
2011-08-08 15:57:16 +0200

    Now the default is to compile to LLVM IR, optionally by supplying -e flag one can use the interpreter

--- Main.hs
+++ Main.hs
@@ -2,14 +2,33 @@ module Main where
 
 
 import GmEvaluator
+import CodeGen
 import System (getArgs)
 import System.Console.GetOpt
 
 
+data Flag = Eval
+    deriving (Eq, Show)
+
+
 main = do
     args <- getArgs
-    let (flags, nonOpts, msgs) = getOpt RequireOrder [] args
+    let (flags, nonOpts, msgs) = getOpt RequireOrder options args
     let [fileName] = nonOpts
     content <- readFile fileName
-    putStrLn . run $ content
+    putStrLn . show $ flags
+    putStrLn . show $ nonOpts
+    case length msgs > 0 of
+        True ->
+            putStrLn . concat $ msgs
+        False ->
+            case Eval `elem` flags of
+                True ->
+                    putStrLn . run $ content
+                False ->
+                    saveLLVMIR . genLLVMIR $ content
+
+
+options :: [OptDescr Flag]
+options = [ Option ['e'] ["eval"] (NoArg Eval) "performs interpretation rather than compile to LLVM bytecode." ]
 
