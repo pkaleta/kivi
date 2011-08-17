04a2263af7b8a1479d8b6416071315d55b9c5524
Piotr Kaleta <piotrek.kaleta@gmail.com>
2011-08-17 16:25:31 +0200

    Added simple integration with Erlang nodes through erl_interface

--- erlang/test.c
+++ erlang/test.c
@@ -8,26 +8,21 @@ extern short erl_thiscreation(void);
 
 const int BUF_SIZE = 1024;
 
-int main() {
-    erl_init(NULL, 0);
-
-    int identification_number = 99;
-    int creation = 1;
-    char *cookie = "secret";
-    if (!erl_connect_init(identification_number, cookie, creation))
-        erl_err_quit("ERROR: erl_connect_init failed");
-
+int kvconnect() {
     int sockfd;
     char *nodename = "pong@kurvinox";
     if ((sockfd = erl_connect(nodename)) < 0)
         erl_err_quit("ERROR: erl_connect failed");
+    return sockfd;
+}
 
+void kvsend(int sockfd) {
     ETERM *arr[2], *ping_msg;
     arr[0] = erl_mk_pid(erl_thisnodename(), sockfd, 0, erl_thiscreation());
     arr[1] = erl_mk_atom("ping");
-    ping_msg = erl_mk_tuple(arr, 2);
 
-    char buf[BUF_SIZE];
+    ping_msg = erl_mk_tuple(arr, 2);
+    unsigned char buf[BUF_SIZE];
     ErlMessage emsg;
     while (1) {
         sleep(1);
@@ -41,6 +36,22 @@ int main() {
     erl_free_term(arr[0]);
     erl_free_term(arr[1]);
     erl_free_term(ping_msg);
+}
 
+
+void erl_interface_init() {
+    erl_init(NULL, 0);
+
+    int identification_number = 99;
+    int creation = 1;
+    char *cookie = "secret";
+    if (!erl_connect_init(identification_number, cookie, creation))
+        erl_err_quit("ERROR: erl_connect_init failed");
+}
+
+int main() {
+    erl_interface_init();
+    kvsend(kvconnect());
     return 0;
 }
+
